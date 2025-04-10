a345b97253b7ff11639a25283f856abe826eeac2
Tejas Singh <138375853+Tejas-Singh5570@users.noreply.github.com>
2025-04-10 10:53:12 +0530

    axios.js
    update cores

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://fullstack-chat-app-1cc3.onrender.com/api";
+
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:5001/api",
-    withCredentials: true,
-})
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
