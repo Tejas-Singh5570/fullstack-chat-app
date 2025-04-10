94abd54aff5bd8b37f106fec39ee8fce130c8960
Tejas Singh <138375853+Tejas-Singh5570@users.noreply.github.com>
2025-04-10 11:42:20 +0530

    Update socket.js
    update cores

--- backend/source/lib/socket.js
+++ backend/source/lib/socket.js
@@ -12,9 +12,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://fullstack-chat-app-1cc3.onrender.com"],
+    credentials: true,
   },
 });
+
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
